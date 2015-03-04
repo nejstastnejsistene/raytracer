39076917f936a06be4cdfcdce92de01af3d61c7b
Peter Johnson <peter@peterjohnson.io>
2015-03-03 22:34:27 -0500

    added specular shading

--- Main.hs
+++ Main.hs
@@ -28,6 +28,7 @@ data Shape = Shape
 
 data Material = Material
     { materialColor       :: Pixel
+    , materialSharpness   :: Integer
     }
 
 data Geometry = Plane Ray
@@ -69,7 +70,7 @@ evaluateRay (Ray origin direction) t = origin + direction ^* t
 
 -- Determine the normal vector of a surface at a point.
 normalVector :: Geometry -> Vector -> Vector
-normalVector (Plane n)           _ = rayDirection n
+normalVector (Plane n)           _ = normalize $ rayDirection n
 normalVector (Triangle p1 p2 p3) _ = normalize $ (p2 - p1) `cross3` (p3 - p1)
 normalVector (Sphere center _)   p = normalize (p - center)
 
@@ -109,14 +110,20 @@ traceRay :: Scene -> Ray -> Pixel
 traceRay s@(Scene bg shapes) r = computeShading (intersect r shapes)
   where
     computeShading [] = bg
-    computeShading (Intersection _ t (Shape m g):_) = (diffuse + specular) ^* intensity
+    computeShading (Intersection _ t s:_) = clamp (total ^* intensity)
       where
         light = normalize (-1,0,1)
         lightIntensity = 1
 
+        Material color sharpness = shapeMaterial s
         ip = evaluateRay r t
-        diffuse = materialColor m ^* (normalVector g ip <.> light)
-        specular = (0,0,0)
+        n = normalize $ normalVector (shapeGeometry s) ip
+        diffuse = clamp $ color ^* (max 0 (n <.> light))
+
+        v = - normalize (rayDirection r)
+        r' = normalize ((2 *^ (n <.> light) *^ n) - light)
+        specular = (1,1,1) ^* (clamp' $ (v <.> r')^sharpness)
+        total = diffuse + specular
 
         ip' = evaluateRay (Ray ip light) (10 * epsilon)
         shadowRay = Ray ip' light
@@ -130,7 +137,7 @@ castRay (Camera pov@(Ray p v) up fl (vw,vh) (imgW,imgH)) (x,y) = ray
   where
     -- Center of view port and right vector.
     center = evaluateRay pov fl
-    right = traceShow (v <.> up) $ normalize (v `cross3` up)
+    right = normalize (v `cross3` up)
     -- Pixel size in terms of the coordinate system.
     pixelWidth = vw / fromIntegral imgW
     pixelHeight = vh / fromIntegral imgH
@@ -155,18 +162,26 @@ renderImage path scene camera = writePPM path imgSize pixelData
   where
     imgSize = cameraResolution camera
     pixelData = map castPixel (renderScene scene camera)
-    castPixel (r,g,b) = (round r, round g, round b)
+    castPixel (r,g,b) = (cast r, cast g, cast b)
+    cast = round . (255*) . clamp'
+
+clamp :: Pixel -> Pixel
+clamp (r,g,b) = (clamp' r,clamp' g, clamp' b)
+
+clamp' :: Float -> Float
+clamp' x | x < 0 = 0
+         | x > 1 = 1
+         | otherwise = x
 
 main :: IO ()
 main = renderImage "out.ppm" scene camera
   where
-    scene = Scene (0xe6,0xff,0xff) (redBall : mirrorBall : chessBoard)
-    redBall    = Shape (Material (255,0,0)) $ Sphere (2,3,0.5) 0.25
-    mirrorBall = Shape (Material (255,255,255)) $ Sphere (4,5,2) 1.5
+    scene = Scene (0.9,1,1) (mirrorBall : chessBoard)
+    mirrorBall = Shape (Material (1,1,1) 150) $ Sphere (4,5,2) 1.5
     chessBoard = concat $ do
         xi <- [0..7]
         yi <- [0..7]
-        let m = Material $ if even xi /= even yi then (0xde,0xbf,0x83) else (0x9f,0x71,0x19)
+        let m = Material (if even xi /= even yi then (0.87,0.75,0.51) else (0.62,0.44,0.10)) 1
             x = fromIntegral xi
             y = fromIntegral yi
             t1 = Triangle (x,y,0) (x+1,y,0) (x,y+1,0)
@@ -174,11 +189,11 @@ main = renderImage "out.ppm" scene camera
         return [Shape m t1, Shape m t2]
     cameraFocus = (4,4,1)
     cameraPos = (1,-2,3)
-    cameraDir@(dx,dy,_) = traceShowId $ normalize $ cameraFocus - cameraPos
-    cameraUp = traceShowId $ normalize $ cameraDir `cross3` traceShowId (-dy,dx,0)
+    cameraDir@(dx,dy,_) = normalize $ cameraFocus - cameraPos
+    cameraUp = normalize $ cameraDir `cross3` (-dy,dx,0)
     camera = Camera { cameraPointOfView  = Ray cameraPos cameraDir
                     , cameraUpDirection  = cameraUp
                     , cameraFocalLength  = 1
                     , cameraViewPortSize = (1.6,0.9)
-                    , cameraResolution   = (320,180)
+                    , cameraResolution   = (640,360)
                     }
