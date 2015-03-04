c0ac70ec67a4632327805131af5f6c73125993d1
Peter Johnson <peter@peterjohnson.io>
2015-03-03 19:52:05 -0500

    fixed vector math, implemented spheres, made a nice scene

--- Main.hs
+++ Main.hs
@@ -8,6 +8,7 @@ import Data.VectorSpace
 import Data.Word
 
 import Debug.Trace
+traceShowId a = trace (show a) a
 
 
 epsilon :: Float
@@ -26,11 +27,12 @@ data Shape = Shape
     }
 
 data Material = Material
-    { materialColor   :: Pixel
+    { materialColor       :: Pixel
     }
 
 data Geometry = Plane Ray
               | Triangle Vector Vector Vector
+              | Sphere Vector Float
 
 data Intersection = Intersection
     { intersectionRay   :: Ray
@@ -63,56 +65,60 @@ normalize v = v ^/ magnitude v
 
 -- Evaluate the point at distance t from the ray's origin.
 evaluateRay :: Ray -> Float -> Vector
-evaluateRay (Ray origin direction) t = origin ^+^ direction ^* t
+evaluateRay (Ray origin direction) t = origin + direction ^* t
 
--- Determine the normal vector of a surface.
-normalVector :: Geometry -> Vector
-normalVector (Plane n) = rayDirection n
-normalVector (Triangle p1 p2 p3) = normalize $ (p2 ^-^ p1) `cross3` (p3 ^-^ p1)
-
+-- Determine the normal vector of a surface at a point.
+normalVector :: Geometry -> Vector -> Vector
+normalVector (Plane n)           _ = rayDirection n
+normalVector (Triangle p1 p2 p3) _ = normalize $ (p2 - p1) `cross3` (p3 - p1)
+normalVector (Sphere center _)   p = normalize (p - center)
 
 intersect :: Ray -> [Shape] -> [Intersection]
 intersect r shapes = sortBy (comparing intersectionTime) intersections
-  where intersections = mapMaybe (intersect' r) shapes
+  where intersections = concatMap (intersect' r) shapes
 
-intersect' :: Ray -> Shape -> Maybe Intersection
-intersect' r s@(Shape m geo) = case intersect'' r geo of
-    Just t -> Just (Intersection r t s)
-    Nothing -> Nothing
+intersect' :: Ray -> Shape -> [Intersection]
+intersect' r s@(Shape m g) = [ Intersection r t s | t <- intersect'' r g ]
 
-intersect'' :: Ray -> Geometry -> Maybe Float
-intersect'' r@(Ray p v) geo = case geo of
-    Plane (Ray off _) -> if hit then Just t else Nothing
+intersect'' :: Ray -> Geometry -> [Float]
+intersect'' r@(Ray p v) g = case g of
+    Plane (Ray off n) -> if hit then [t] else []
       where
         cos = v <.> n
-        t = ((off ^-^ p) <.> n) / cos
+        t = ((off - p) <.> n) / cos
         hit = abs cos > epsilon && t > -epsilon
-    Triangle p1 p2 p3 -> case intersect'' r (Plane (Ray p1 n)) of
-        Nothing -> Nothing
-        Just t -> if hit then Just t else Nothing
+    Triangle p1 p2 p3 -> case intersect'' r $ Plane (Ray p1 n) of
+        [] -> []
+        [t] -> if hit then [t] else []
           where
             q = evaluateRay r t
-            a = (p2^-^q) `cross3` (p3^-^q) <.> n
-            b = (p3^-^q) `cross3` (p1^-^q) <.> n
-            g = (p1^-^q) `cross3` (p2^-^q) <.> n
-            hit = a > -epsilon && b > -epsilon && g > -epsilon
-  where
-    n = normalVector geo
+            a  = (p2-q) `cross3` (p3-q) <.> n
+            b  = (p3-q) `cross3` (p1-q) <.> n
+            g' = (p1-q) `cross3` (p2-q) <.> n
+            hit = a > -epsilon && b > -epsilon && g' > -epsilon
+      where n = normalVector g p1 
+    Sphere center radius -> [ t | t <- [tMid - dt, tMid + dt], d2 < r2 && t > -epsilon ] 
+      where
+        l = center - p -- Vector torwards the center.
+        tMid = l <.> v -- Distance to nearest point on ray the center.
+        d2 = l <.> l - tMid * tMid -- Distance from ray to center squared.
+        r2 = radius * radius
+        dt = sqrt (r2 - d2)
 
 traceRay :: Scene -> Ray -> Pixel
 traceRay s@(Scene bg shapes) r = computeShading (intersect r shapes)
   where
     computeShading [] = bg
-    computeShading (Intersection _ t (Shape m g):_) = (diffuse ^+^ specular) ^* intensity
+    computeShading (Intersection _ t (Shape m g):_) = (diffuse + specular) ^* intensity
       where
-        light = (0,0,0) ^-^ normalize (1,-1,-1)
+        light = normalize (-1,0,1)
         lightIntensity = 1
 
         ip = evaluateRay r t
-        diffuse = materialColor m ^* (normalVector g <.> light)
+        diffuse = materialColor m ^* (normalVector g ip <.> light)
         specular = (0,0,0)
 
-        ip' = evaluateRay (Ray ip light) (2 * epsilon)
+        ip' = evaluateRay (Ray ip light) (10 * epsilon)
         shadowRay = Ray ip' light
         intensity = case intersect shadowRay shapes of
             [] -> lightIntensity
@@ -122,11 +128,9 @@ traceRay s@(Scene bg shapes) r = computeShading (intersect r shapes)
 castRay :: Camera -> (Integer,Integer) -> Ray
 castRay (Camera pov@(Ray p v) up fl (vw,vh) (imgW,imgH)) (x,y) = ray
   where
-    -- Center of view port.
+    -- Center of view port and right vector.
     center = evaluateRay pov fl
-    -- Vectors down and to the right along the view port.
-    down  = normalize (center ^+^ (-2) *^ up)
-    right = normalize (v `cross3` up)
+    right = traceShow (v <.> up) $ normalize (v `cross3` up)
     -- Pixel size in terms of the coordinate system.
     pixelWidth = vw / fromIntegral imgW
     pixelHeight = vh / fromIntegral imgH
@@ -134,8 +138,8 @@ castRay (Camera pov@(Ray p v) up fl (vw,vh) (imgW,imgH)) (x,y) = ray
     hOff = (fromIntegral x + 0.5) * pixelWidth - vw / 2
     vOff = (fromIntegral y + 0.5) * pixelWidth - vh / 2
     -- Point in view port through which to cast the ray.
-    p'= center ^+^ (down^*vOff) ^+^ (right^*hOff)
-    ray = Ray p $ normalize (p' ^-^ p)
+    p' = center - up^*vOff + right^*hOff
+    ray = Ray p $ normalize (p' - p)
 
 -- Render a scene from a camera into an array of pixels.
 renderScene :: Scene -> Camera -> [Pixel]
@@ -156,18 +160,25 @@ renderImage path scene camera = writePPM path imgSize pixelData
 main :: IO ()
 main = renderImage "out.ppm" scene camera
   where
-    ground = Shape (Material (255,255,255)) $ Plane $ Ray (0,0,-1) (0,0,1)
-    triangles = do
-        y <- map (/3) [0..3.0]
-        let m = Material (255,0,0)
-        return $ Shape m $ Triangle (2,2-2*y,-1) (1,1-3*y,-1) (1.5,1.5-2*y,1)
-    scene = Scene { sceneBackground = (0,0,0)
-                  , sceneShapes     = ground : triangles
-                  }
-    -- Camera POV is behind the YZ-plane, with the Z-axis as up.
-    camera = Camera { cameraPointOfView  = Ray (-1,0,0) (1,0,0)
-                    , cameraUpDirection  = (0,0,1)
+    scene = Scene (0xe6,0xff,0xff) (redBall : mirrorBall : chessBoard)
+    redBall    = Shape (Material (255,0,0)) $ Sphere (2,3,0.5) 0.25
+    mirrorBall = Shape (Material (255,255,255)) $ Sphere (4,5,2) 1.5
+    chessBoard = concat $ do
+        xi <- [0..7]
+        yi <- [0..7]
+        let m = Material $ if even xi /= even yi then (0xde,0xbf,0x83) else (0x9f,0x71,0x19)
+            x = fromIntegral xi
+            y = fromIntegral yi
+            t1 = Triangle (x,y,0) (x+1,y,0) (x,y+1,0)
+            t2 = Triangle (x+1,y+1,0) (x,y+1,0) (x+1,y,0)
+        return [Shape m t1, Shape m t2]
+    cameraFocus = (4,4,1)
+    cameraPos = (1,-2,3)
+    cameraDir@(dx,dy,_) = traceShowId $ normalize $ cameraFocus - cameraPos
+    cameraUp = traceShowId $ normalize $ cameraDir `cross3` traceShowId (-dy,dx,0)
+    camera = Camera { cameraPointOfView  = Ray cameraPos cameraDir
+                    , cameraUpDirection  = cameraUp
                     , cameraFocalLength  = 1
-                    , cameraViewPortSize = (2.1,2.1)
-                    , cameraResolution   = (1000,1000)
+                    , cameraViewPortSize = (1.6,0.9)
+                    , cameraResolution   = (320,180)
                     }
